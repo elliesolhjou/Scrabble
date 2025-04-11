884a45465235278deec7f6d2dcf52929956a49ea
Ellie Solhjou <elliesolhjou@gmail.com>
2025-04-11 14:47:55 -0700

    Completed AnagramDictionary

--- AnagramDictionary.java
+++ AnagramDictionary.java
@@ -4,7 +4,9 @@
 // Spring 2025
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.*;
+import java.io.File;
+
 
 
 /**
@@ -14,21 +16,62 @@ import java.util.ArrayList;
    letters too, and likewise if the dictionary words are all upper case.
  */
 public class AnagramDictionary {
-   
+   // Representation Invariant:
+   // - anagramMap != null
+   // - No null keys or values in anagramMap
+   // - All keys in anagramMap are sorted lowercase strings (canonical forms of words)
+   // - All values are non-empty ArrayLists of real dictionary words (2 or more characters)
+   // - wordSet != null
+   // - wordSet contains no duplicates and only words of length >= 2
 
+   private Map<String, ArrayList<String>> anagramMap;
+   private Set<String> wordSet;
 
    /**
-      Create an anagram dictionary from the list of words given in the file
-      indicated by fileName.  
-      @param fileName  the name of the file to read from
-      @throws FileNotFoundException  if the file is not found
-      @throws IllegalDictionaryException  if the dictionary has any duplicate words
-    */
-   public AnagramDictionary(String fileName) throws FileNotFoundException,
-                                                    IllegalDictionaryException {
+   Create an anagram dictionary from the list of words given in the file
+   indicated by fileName.  
+   @param fileName  the name of the file to read from
+   @throws FileNotFoundException  if the file is not found
+   @throws IllegalDictionaryException  if the dictionary has any duplicate words
+   */
+   public AnagramDictionary(String fileName) throws FileNotFoundException, IllegalDictionaryException{
+      anagramMap = new HashMap<>();
+      wordSet = new HashSet<>();
 
+      File file = new File(fileName);
+      Scanner in = new Scanner(file);
+
+      while (in.hasNext()){
+         String word = in.next();
+         if (word.length() == 0){
+            continue;
+         }
+         // Check for duplicates
+         if (wordSet.contains(word)){
+            throw new IllegalDictionaryException("ERROR: Illegal dictionary: dictionary file has a duplicate word:" + word);
+         }
+         wordSet.add(word);
+
+         // Sort word in canonical form
+         char[] chars = new char[word.length()];
+         for(int i=0; i< word.length(); i++){
+            chars[i] = word.charAt(i);
+         }
+         Arrays.sort(chars);
+         // Convert to string
+         String canonical = new String(chars);  
+
+         // Add to anagramMap
+         if(!anagramMap.containsKey(canonical)){
+            ArrayList<String> keyList = new ArrayList<>();
+            anagramMap.put(canonical, keyList);
+
+            anagramMap.get(canonical).add(word);
+         }
+      }
+      in.close();
    }
-   
+
 
    /**
       Get all anagrams of the given string. This method is case-sensitive.
@@ -37,7 +80,17 @@ public class AnagramDictionary {
       @return a list of the anagrams of s
     */
    public ArrayList<String> getAnagramsOf(String string) {
-      return new ArrayList<String>(); // DUMMY CODE TO GET IT TO COMPILE
+      char[] chars = new char[string.length()];
+      for (int i = 0; i< string.length(); i++){
+         chars[i] = string.charAt(i);
+      }
+      Arrays.sort(chars);
+      String canonical = new String(chars);
+      ArrayList<String> anagramList = anagramMap.get(canonical);
+      if(anagramList == null){
+         return new ArrayList<String>();
+      }
+      return new ArrayList<String>(anagramList);
    }
    
    
--- WordFinder.java
+++ WordFinder.java
@@ -2,3 +2,39 @@
 // USC NetID: 
 // CS 455 PA4
 // Spring 2025
+
+import java.util.*;
+
+public class WordFinder{
+
+    public static void main(String[] args){
+
+        System.out.println("Type . to quit.");
+        Scanner in = new Scanner(); 
+
+        while(in.hasNext() && (in.next() != '.' || in.next() != "\n")){
+            try{
+                System.out.print("Rack?");
+                Rack rack = in.next();
+                //read rack
+                //process rack
+            }
+            catch()
+            {
+
+            }
+
+        }
+        //classname class = readInput(in);
+        //validateinput(class);
+        //validate is static private method here
+        
+        //after validation
+        if (in == '.'){
+            exit 0;
+        }
+        System.out.printf("We can make %d words from "%s"",count, in );
+        System.out.println("All of the words with their scores (sorted by score):");
+        //System.out.println(//score : scrablled word);
+    }
+}
